e7cc2ab19ee1e506da057018094374515b793467
Zafar343 <zafar343@chungbuk.ac.kr>
2024-11-14 04:15:49 +0000

    yolov11 training, validation and testing code added

---
+++ train.py
@@ -0,0 +1,50 @@
+from ultralytics import YOLO
+import argparse
+import torch
+
+def train_yolov8():
+    args   = parseArgs()
+    # choose between training from scratch and training using a checkpoint
+    device = torch.device(f"cuda:{args.device}" if torch.cuda.is_available() else "cpu")
+    if args.config:
+        model_type = args.config
+    else:
+        model_type = args.model
+    # Load YOLO model    
+    model = YOLO(model_type)
+
+    # Train model
+    model.train(
+        data        = args.data       ,
+        epochs      = args.epochs     ,                   
+        batch       = args.batch_size ,         
+        imgsz       = args.img_size   ,         
+        lr0         = args.initial_lr ,                  
+        project     = args.project    ,       
+        device      = device          ,
+        save_period = args.save_period        
+    )
+
+def parseArgs():
+    parser = argparse.ArgumentParser(description='yolo training')
+    parser.add_argument("--model"      , type=str, default='yolov11n.pt',
+                        help='provide a model path (model.pt) if initializing from a pretrained checkpoint'  )
+    parser.add_argument("--config"     , type=str, default=None,
+                        help='provide a model config filename (.yaml) if training from scratch'              )
+    parser.add_argument("--data"       , type=str, default='data.yaml', help='provide data.yaml path'        )
+    parser.add_argument("--epochs"     , type=int, default=100, help='specify the number of epochs'          )
+    parser.add_argument("--batch_size" , type=int, default=32, help='specify the batch size'                 )
+    parser.add_argument("--img_size"   , type=int, default=640, help='specify the image size'                )
+    parser.add_argument("--project"    , type=str, default='runs/membrane_crack', help='name your project'   )
+    parser.add_argument("--name"       , type=str, default=''                                                )
+    parser.add_argument("--device"     , type=int, default=0, help='Chosse a gpu device'                     )
+    parser.add_argument("--initial_lr" , type=float, default=0.01                                            )
+    parser.add_argument("--save_period", type=int, default=-1,
+                        help='Save checkpoint every x epochs (disabled if < 1).'                             )
+
+    args = parser.parse_args()
+    return args
+
+if __name__ == "__main__":
+    # Run training
+    train_yolov8()
---
+++ val.py
@@ -0,0 +1,41 @@
+from ultralytics import YOLO
+import argparse
+import torch
+
+def validate():
+    args = parseArgs()
+    # choose between training from scratch and training using a checkpoint
+    device = torch.device(f"cuda:{args.device}" if torch.cuda.is_available() else "cpu")
+    # Load YOLO model    
+    model = YOLO(args.model)
+
+    # Train model
+    model.val(
+        data    = args.data       ,          
+        batch   = args.batch_size ,         
+        imgsz   = args.img_size   ,                           
+        project = args.project    ,       
+        device  = device          ,
+        save_txt= args.save_txt   ,
+        conf    = args.conf       
+    )
+
+def parseArgs():
+    parser = argparse.ArgumentParser(description='Yolov8 validation'                                       )
+    parser.add_argument("--model"      , type=str, default='',
+                        help='provide a model path (model.pt) if initializing from a pretrained checkpoint')
+    parser.add_argument("--data"       , type=str, default='data.yaml', help='provide data.yaml path'      )
+    parser.add_argument("--batch_size" , type=int, default=16, help='specify the batch size'               )
+    parser.add_argument("--img_size"   , type=int, default=640, help='specify the image size'              )
+    parser.add_argument("--project"    , type=str, default='runs/Yolov8_Tutorial', help='name your project')
+    parser.add_argument("--name"       , type=str, default='custom_training'                               )
+    parser.add_argument("--device"     , type=int, default=0, help='Chosse a gpu device'                   )
+    parser.add_argument("--save_txt"   , action='store_true'                                               )
+    parser.add_argument("--conf"       , type=float, default=0.25                                          )
+
+    args = parser.parse_args()
+    return args
+
+if __name__ == "__main__":
+    # Run training
+    validate()
